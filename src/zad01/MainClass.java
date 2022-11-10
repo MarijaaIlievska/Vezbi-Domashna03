36667003b795f09ed6bf921643372f1c1805fbbe
Marija <marija.ilievska4@gmail.com>
2022-11-10 21:59:06 +0100

    First commit

--- src/zad01/MainClass.java
+++ src/zad01/MainClass.java
@@ -2,7 +2,7 @@ package zad01;
 
 public class MainClass {
 	public static void main(String[] args) {
-		Avtomobil av = new Avtomobil("Mercedes-Benz", "C220", "Siva", 12400, 2012, "BT-0000-AA" );
+		Avtomobil av = new Avtomobil("Toyota", "Corolla", "Bela", 12400, 2012, "BT-0000-AA" );
 		
 		av.prvMetod();
 		System.out.println(av.vtorMetod());
